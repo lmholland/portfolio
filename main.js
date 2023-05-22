30a12ca8ab4f98774d98f428d2eba9442ec3496e
lmholland <lmholland@proton.me>
2023-05-22 20:12:51 +0900

    added typewriter

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 (() => {
     window.addEventListener("click", clickFunctions);
+    typewriter();
+
+    function typewriter(){
+        let title = document.getElementById("title").innerHTML;
+        let jobs = ["Software Developer", "World Traveler", "Geography Nerd"]
+        let currentLetter = 0;
+        let currentJob = 0;
+        setInterval(function typewriter(){
+            if(jobs[currentJob].length == currentLetter){
+                if(currentJob == jobs.length-1){
+                    currentJob = 0;
+                } else {
+                    currentJob += 1;
+                }
+                currentLetter = 0;
+            }
+            currentLetter+=1;
+            let jobLength = jobs[currentJob].length;
+            let partial = jobs[currentJob].slice(0, jobLength-(jobLength-currentLetter))
+            document.getElementById("title").innerHTML = partial
+        }, 150)
+    }
 
     function clickFunctions(){
         toggleOff();
