132f54b47156e9bb2e641612cd643dc912051fd7
lmholland <lmholland@proton.me>
2023-05-25 10:32:29 +0900

    wikiscraper drowdown content

--- main.js
+++ main.js
@@ -4,25 +4,43 @@
     window.addEventListener("click", clickFunctions);
 
     function typewriter(){
-        let jobs = ["World Traveler", "Geography Nerd", "Software Developer", "Based in Seoul, South Korea",]
+        let jobs = ["World Traveler", "Geography Nerd", "Software Developer"]
         let currentLetter = 0;
         let currentJob = 0;
-        let pause = 7; // intervals
+        let pause = 15; // intervals
+        let blinks = 0;
+        let blinker = document.createElement("span");
+
+        blinker.setAttribute("opacity", "1");
+        blinker.innerHTML = "|"
 
         setInterval(function typewriter(){
             if(jobs[currentJob].length + pause == currentLetter){
                 if(currentJob == jobs.length-1){
                     currentJob = 0;
                 } else {
-                    currentJob += 1;
+                    currentJob++;
                 }
                 currentLetter = 0;
             }
-            currentLetter += 1;
+
+            currentLetter++;
+            blinks++
+
+            if(blinks > 6){
+                blinker.style.opacity = 1;
+                if(blinks > 9){
+                    blinks = 0;
+                }
+            } else {
+                blinker.style.opacity = 0;
+            }
+
             let jobLength = jobs[currentJob].length;
-            let partial = jobs[currentJob].slice(0, jobLength-(jobLength-currentLetter))
-            document.getElementById("title").innerHTML = partial
-        }, 150)
+            let partial = jobs[currentJob].slice(0, jobLength-(jobLength-currentLetter));
+            document.getElementById("title").innerHTML = partial;
+            document.getElementById("title").appendChild(blinker);
+        }, 100)
     }
 
     function clickFunctions(){
